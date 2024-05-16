e036eb8a74d08e08fcd47a13c0dec1cf38864bac
Dean Poulos <dean.poulos7@gmail.com>
2024-05-16 22:04:17 +1000

    Attempt fix for package root directory.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/qua-platform/QuaQsim",
     packages=setuptools.find_packages(),
-    package_dir={'': 'quaqsim'},
+    # package_dir={'': ''},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
