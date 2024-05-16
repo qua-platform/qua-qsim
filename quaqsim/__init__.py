11fc535c34f41240d93fac669250a3e605d42316
Dean Poulos <dean.poulos7@gmail.com>
2024-05-16 23:15:32 +1000

    Adjust setup.

--- quaqsim/__init__.py
+++ quaqsim/__init__.py
@@ -1,3 +1,3 @@
-from .program_to_quantum_pulse_sim_compiler.quantum_pulse_sim_compiler import Compiler
-from .simulate import simulate_program
+from program_to_quantum_pulse_sim_compiler.quantum_pulse_sim_compiler import Compiler
+from simulate import simulate_program
 from . import architectures, program_ast, program_dict_to_program_compiler, program_to_quantum_pulse_sim_compiler
