8b7f824e543adb02241a64bfe863214683dc8f9e
Serwan Asaad <serwan.asaad@quantum-machines.co>
2025-05-19 21:52:06 +0200

    Create version without qiskit dependency

--- quaqsim/__init__.py
+++ quaqsim/__init__.py
@@ -1,3 +1,8 @@
-from .program_to_quantum_pulse_sim_compiler.quantum_pulse_sim_compiler import Compiler
-from .simulate import simulate_program
-from . import architectures, program_ast, program_dict_to_program_compiler, program_to_quantum_pulse_sim_compiler
+# from .program_to_quantum_pulse_sim_compiler.quantum_pulse_sim_compiler import Compiler
+# from .simulate import simulate_program
+from . import (
+    architectures,
+    program_ast,
+    program_dict_to_program_compiler,
+    program_to_quantum_pulse_sim_compiler,
+)
